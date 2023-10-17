c2ba01d6278620b69ba3e5b9e114e9eb681b0167
V.Alexandrovich <beloenko23@gmail.com>
2023-10-17 18:14:06 +0300

    ProductV View

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import LoginView from '@/views/LoginView.vue'
 import DashboardView from "@/views/DashboardView.vue";
 import AccountView from "@/views/AccountView.vue";
+import ProductView from "@/views/ProductView.vue";
 
 const router = createRouter({
   history: createWebHistory('/augmento-admin/'),
@@ -20,6 +21,16 @@ const router = createRouter({
       path: '/account',
       name: 'account',
       component: AccountView
+    },
+    {
+      path: '/product/:id',
+      name: 'product-edit',
+      component: ProductView
+    },
+    {
+      path: '/product',
+      name: 'product-create',
+      component: ProductView
     }
   ]
 })
---
+++ src/views/ProductView.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+
+import {onMounted, ref} from "vue";
+import {useRoute} from "vue-router";
+
+const productId = ref(null);
+
+const route = useRoute();
+
+onMounted(()=>{
+  console.log('Mounted')
+  console.log(route.params.id)
+
+  if (route.name == 'product-edit'){
+    productId.value = route.params.id;
+    console.log('edit')
+  } else if (route.name == 'product-create'){
+    console.log('new')
+  }
+
+})
+
+</script>
+
+<template>
+<div class="p-32">
+  Product page
+  <p class="text-4xl font-bold">{{productId}}</p>
+</div>
+</template>
+
+<style scoped>
+
+</style>

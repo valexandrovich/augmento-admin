1320a5adcc343679bf2a2dd760738fbc7e85b31e
V.Alexandrovich <beloenko23@gmail.com>
2023-10-17 14:03:01 +0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import HeaderComponent from "@/components/HeaderComponent.vue";
+import FooterComponent from "@/components/FooterComponent.vue";
+</script>
+
+<template>
+  <div class="h-screen flex flex-col">
+    <HeaderComponent/>
+    <div class=" flex-grow" style="background-color: #F6F6F6">
+        <RouterView/>
+    </div>
+    <FooterComponent/>
+  </div>
+</template>
+
+<style scoped>
+
+</style>

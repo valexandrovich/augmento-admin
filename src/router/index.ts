735b693ff9591b873578ec1aa9c4ba17571dabb1
V.Alexandrovich <beloenko23@gmail.com>
2023-10-17 14:44:41 +0300

    Base path in Vue

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,7 +3,9 @@ import LoginView from '@/views/LoginView.vue'
 import DashboardView from "@/views/DashboardView.vue";
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory('./'),
+  // history: createWebHistory(import.meta.env.BASE_URL),
+  // history: createWebHistory(process.env.BASE_URL),
   routes: [
     {
       path: '/login',

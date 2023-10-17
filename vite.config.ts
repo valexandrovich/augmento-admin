3fafebc5b744002d93785c028635f7b7a9a44159
V.Alexandrovich <beloenko23@gmail.com>
2023-10-17 15:11:02 +0300

    Base path in env

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,15 @@
 import { fileURLToPath, URL } from 'node:url'
 
+
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 
 export default defineConfig({
-  base: './',
+  // base: './',
   // base: '/augmento-admin/',
   // base: import.meta.env.VITE_BASE_URL || './',
+  base: process.env.BASE_URL,
   plugins: [
     vue(),
     vueJsx(),
